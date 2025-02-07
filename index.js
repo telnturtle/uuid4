ba1db6f66a7ef55e9cab209041b4f36475a1a9ad
엄휘용 <telnturtle@gmail.com>
2025-02-07 20:01:04 +0900

    UUID 생성 및 복사 기능 개선, 히스토리 표시 기능 추가

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const handleClickRandom = async () => {
   const uuid = crypto.randomUUID()
   document.getElementById('output').innerText = uuid
+
+  // render history uuids
+  uuids.push(uuid)
+  const history = document.querySelector('.history')
+  history.innerHTML = ''
+  uuids.forEach((uuid) => {
+    const li = document.createElement('li')
+    li.innerText = uuid
+    history.appendChild(li)
+  })
 }
 
 const copiedTimer = {
@@ -18,18 +28,36 @@ const handleClickCopy = () => {
   }, 2000)
 }
 
+const uuids = []
+const historyEl = {
+  opened: false,
+}
+const historyToggledTimer = {
+  timer: null,
+}
+const handleClickHistory = () => {
+  historyEl.opened = !historyEl.opened
+  const history = document.querySelector('.history')
+  if (historyEl.opened) {
+    history.style.display = 'block'
+  } else {
+    history.style.display = 'none'
+  }
+
+  const button = document.querySelector('#history')
+  button.classList.add('copied')
+  historyToggledTimer && window.clearTimeout(historyToggledTimer)
+  historyToggledTimer = window.setTimeout(() => {
+    button.classList.remove('copied')
+  }, 2000)
+}
+
 const handlers = {
   random: handleClickRandom,
   copy: handleClickCopy,
+  history: handleClickHistory,
 }
 
 for (const [key, handler] of Object.entries(handlers)) {
   document.getElementById(key).addEventListener('click', handler)
 }
-
-const handleKeydownInput = (event) => {
-  if (event.key === 'Enter') {
-    handleClickRandom()
-  }
-}
-document.getElementById('input').addEventListener('keydown', handleKeydownInput)
